449397a35945a1ef64aad9b118746eccadb53a91
Lakshit Paliwal <39520168+LakshitPaliwal@users.noreply.github.com>
2020-03-06 21:29:53 +0530

    Update Newpage.js

--- Components/Newpage.js
+++ Components/Newpage.js
@@ -2,10 +2,7 @@ import React,{Component} from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 
 class Newpage extends Component{
@@ -13,32 +10,30 @@ render(){
 return <div>
       <Typography component="h1" variant="h5">
            Sign up
-           <Link color="inherit" href="https://lakshit27.github.io.com/">
+           <Link color="inherit" href="https://lakshit27.github.io">
         &nbsp;Your Website
            </Link>
-      </Typography> <Toolbar>
+      </Typography>
+      <Toolbar>
           <Typography variant="h6" color="inherit" noWrap>
             Company name
           </Typography>
-        </Toolbar>
-
-     
-        <Toolbar position="absolute" color="default" >
+      </Toolbar>
+      
+     <Toolbar position="absolute" color="default" >
           <Typography variant="h6" color="inherit" noWrap>
             Company name
           </Typography>
-        </Toolbar>
-        <AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
+      </Toolbar>
+        
+  <Toolbar position="static">
+   
+    <Typography variant="h6" >
       News
     </Typography>
     <Button color="inherit">Login</Button>
   </Toolbar>
-</AppBar>
+
     </div>
 }
 }
